0ed88253d72c765243f2eba521e281b60ba9f229
rrilik <66564020+rrilik@users.noreply.github.com>
2021-12-28 21:31:21 +0200

    made commits
    visualization and eating enemies

--- client.py
+++ client.py
@@ -2,19 +2,19 @@ import socket
 import  time
 import pygame
 
-def draw_opponents(data):
+WIDTH = 1000
+HEIGHT = 800
+colours = {'0': (255, 255, 0), '1': (255, 0, 0), '2': (0, 255, 0),
+           '3':(0, 255,255), '4': (255, 34, 178)}
+def draw_opponents(data, screen):
     for i in range(len(data)):
-        j=data[i].split(' ')
+        j = data[i].split(' ')
 
-    x = WIDTH//2 + int(j[0])
-    y = HEIGHT//2 + int(j[1])
-    r=int(j[2])
-    c=j[3]
-    pygame.draw.circle(sc ,c, (x,y), r)
-
-def new_radius(R, r):
-    return (R**2+r**2)**0.5
-
+        x = WIDTH // 2 + int(j[0])
+        y = HEIGHT // 2 + int(j[1])
+        r = int(j[2])
+        c = colours[j[3]]
+        pygame.draw.circle(screen, c, (x, y), r)
 def find(strn):
     first_o = None
     for i in range(len(strn)):
@@ -23,37 +23,15 @@ def find(strn):
         if strn[i] == '>' and first_o != None:
             close = i
             res = strn[first_o + 1: close]
-            res = list(map(int, res.split(',')))
             return res
     return ''
 
 
-WIDTH = 1000
-HEIGHT = 800
-colours = {'0': (255, 255, 0), '1': (255, 0, 0), '2': (0, 255, 0),
-           '3':(0, 255,255), '4': (255, 34, 178)}
-
-class Me():
-    def __init__(self, data):
-        data=data.split()
-        self.r=int(data[0])
-        self.colour=data[1]
-
-    def update(self, new_r):
-        self.r = new_r
-
-    def draw(self):
-        if self.r != 0:
-            pygame.draw.circle(sc, colours[self.colour], (WIDTH // 2, HEIGHT // 2), self.r)
-
 local_s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 local_s.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
 local_s.connect(('localhost', 10000))
-local_s.recv(16).decode()
 
-
-data=local_s.recv(64).decode()
-me = Me(data)
+my_colour = local_s.recv(16).decode()
 
 pygame.init()
 sc = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -70,7 +48,7 @@ while is_game_running:
     if pygame.mouse.get_focused():
         pos = pygame.mouse.get_pos()
         v = [pos[0] - WIDTH//2, pos[1]-HEIGHT//2]
-        if v[0]**2 + v[1]**2 <= me.r**2:
+        if v[0]**2 + v[1]**2 <= 50**2:
             v = [0, 0]
 
     if len(v) > 0 and v != old_v:
@@ -83,13 +61,13 @@ while is_game_running:
     data = local_s.recv(1024)
     data = data.decode()
     data = find(data)
+    print('receive', data)
     data = data.split(',')
 
     sc.fill('grey50')
+    pygame.draw.circle(sc, colours[my_colour], (WIDTH//2, HEIGHT//2), 50)
+    if data!=['']:
+        draw_opponents(data, sc)
     pygame.display.update()
-    if data !=['']:
-        me.update(int(data[0]))
-        draw_opponents(data[1:])
-        me.draw()
 pygame.quit()
 #python client.py
--- server.py
+++ server.py
@@ -1,15 +1,11 @@
 import socket
 import random
 import pygame
-from client import new_r
 
 WIDTH_ROOM, HEIGHT_ROOM = 5000, 5000
 WIDTH_SERVER_WINDOW, HEIGHT_SERVER_WINDOW = 300, 300
 FPS = 100
-START_SIZE = 20
-MOBS_QUANTITY = 25
-FOOD_SIZE = 15
-FOOD_QUANTITY = WIDTH_ROOM*HEIGHT_ROOM//100000
+START_SIZE = 50
 colours = {'0': (255, 255, 0), '1': (255, 0, 0), '2': (0, 255, 0),
            '3':(0, 255,255), '4': (255, 34, 178)}
 def find(strn):
@@ -24,15 +20,6 @@ def find(strn):
             return res
     return  ''
 
-
-class Food():
-    def __init__(self,x,y,r,colour):
-        self.x=x
-        self.y=y
-        self.r=r
-        self.colour=colour
-
-
 class Player():
     def __init__(self, conn, addr, x, y, r, colour):
         self.conn = conn
@@ -44,13 +31,12 @@ class Player():
         self.w_vision = 1000
         self.h_vision = 800
         self.errors = 0
-        self.abs_speed = 50/(self.r**0.5)
+        self.abs_speed = 1
         self.speed_x = 0
         self.speed_y = 0
     def change_speed(self, v):
         if v[0] == 0 and v[1] == 0:
-            self.x = 0
-            self.y = 0
+            pass
         else:
             len_vector = (v[0]**2 + v[1]**2) ** 0.5
             v = (v[0]/len_vector, v[1]/len_vector)
@@ -58,28 +44,27 @@ class Player():
             self.speed_x, self.speed_y = v[0], v[1]
 
     def update(self):
-        if self.x-self.r<=0:
-            if self.speed_x>=0:
-                self.x+=self.speed_x
+        if self.x - self.r <= 0:
+            if self.speed_x >= 0:
+                self.x += self.speed_x
         else:
-            if self.x+self.r>= WIDTH_ROOM:
-                if self.speed_x<=0:
-                    self.x+=self.speed_x
+            if self.x + self.r >= WIDTH_ROOM:
+                if self.speed_x <= 0:
+                    self.x += self.speed_x
             else:
-                self.x+=self.speed_x
+                self.x += self.speed_x
 
-        if self.y-self.r<=0:
-            if self.speed_y>=0:
-                self.y+=self.speed_y
+        if self.y - self.r <= 0:
+            if self.speed_y >= 0:
+                self.y += self.speed_y
         else:
-            if self.y+self.r>= HEIGHT_ROOM:
-                if self.speed_y<=0:
-                    self.y+=self.speed_y
+            if self.y + self.r >= HEIGHT_ROOM:
+                if self.speed_y <= 0:
+                    self.y += self.speed_y
             else:
-                self.y+=self.speed_y
+                self.y += self.speed_y
 
-        self.abs_speed = 50/(self.r**0.5)
-
+        #self.abs_speed = 50 / (self.r ** 0.5)
 
 
 main_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -88,140 +73,87 @@ main_socket.bind(('localhost', 10000))
 main_socket.setblocking(0)
 main_socket.listen(5)
 
-
-players = [Player(None, None,
-                  random.randint(0,WIDTH_ROOM),
-                  random.randint(0,HEIGHT_ROOM),
-                  random.randint(10,100),
-                  str(random.randint(0,4))
-                  )
-    for i in range(MOBS_QUANTITY)]
-
-microbes=[Food(random.randint(0,WIDTH_ROOM),
-                  random.randint(0,HEIGHT_ROOM),
-                  FOOD_SIZE,
-                  str(random.randint(0,4)))
-    for i in range(FOOD_QUANTITY)]
-
-tick=-1
+players = []
 is_server_running = True
 pygame.init()
 sc = pygame.display.set_mode((WIDTH_SERVER_WINDOW, HEIGHT_SERVER_WINDOW))
 pygame.display.set_caption('Agario_Server')
 clock = pygame.time.Clock()
 while is_server_running:
-    tick+=1
     clock.tick(FPS)
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
             is_server_running = False
-    if tick==200:
-        tick=0
-        try:
-            new_socket, address = main_socket.accept()
-            print("NewPlayer", address)
-            new_socket.setblocking(0)
-            new_player = Player(new_socket, address,
-                                random.randint(0, WIDTH_ROOM),
-                                random.randint(0, HEIGHT_ROOM),
-                                START_SIZE, str(random.randint(0,4)))
-            message = str(new_player.r)+' '+new_player.colour
-            new_player.conn.sned(message.encode())
-            players.append(new_player)
+    try:
+        new_socket, address = main_socket.accept()
+        print("NewPlayer", address)
+        new_socket.setblocking(0)
+        new_player = Player(new_socket, address,
+                            random.randint(0, WIDTH_ROOM),
+                            random.randint(0, HEIGHT_ROOM),
+                            START_SIZE, str(random.randint(0,4)))
+        new_player.conn.send(new_player.colour.encode())
+        players.append(new_player)
 
-        except:
-            print("NoAvailable")
+    except:
+        pass
 
     for p in players:
-        if p.conn!=0:
-            try:
-                data = p.conn.recv(1024)
-                data = data.decode()
-                data = find(data)
-                print(data)
+        try:
+            data = p.conn.recv(1024)
+            data = data.decode()
+            data = find(data)
+            print(data)
 
-                p.change_speed(data)
-            except:
-                pass
-        else:
-            if tick ==100:
-                data=[random.randint(-50,50),random.randint(-50,50)]
-                p.change_speed(data)
+            p.change_speed(data)
+        except:
+            pass
         p.update()
 
     visible_players = [[] for i in range(0,len(players))]
     for i in range(len(players)):
-        for k in range(len(microbes)):
-            dist_x = microbes[k].x - players[i].x
-            dist_y = microbes[k].y - players[i].y
-            if (
-                    (abs(dist_x) <= (players[i].w_vision) // 2 + microbes[k].r) and
-                    (abs(dist_y) <= (players[i].h_vision) // 2 + microbes[k].r)):
-                if (dist_x ** 2 + dist_y ** 2) ** 0.5 <= players[i].r and players[i].r > 1.1 * players[j].r:
-                    players[i].r = new_r(players[i].r, microbes[k].r)
-                    microbes[k].r=0
-
-                if (players[i].conn!=None) and (microbes[k].r!=0):
-                    _x = str(round(dist_x))
-                    _y = str(round(dist_y))
-                    _r = str(round(microbes[k].r))
-                    _c = microbes[k].colour
-
-                    visible_players[i].append(_x + ' ' + _y + ' ' + _r + ' ' + _c)
-
         for j in range(i+1, len(players)):
             dist_x = players[j].x - players[i].x
             dist_y = players[j].y - players[i].y
 
-            if (
-                (abs(dist_x)<= (players[i].w_vision)//2+players[j].r) and
-                (abs(dist_y)<= (players[i].h_vision)//2+players[j].r)):
-                if (dist_x**2 +dist_y**2)**0.5 <= players[i].r and players[i].r > 1.1*players[j].r:
-                    players[i].r = new_r(players[i].r, players[j].r)
-                    players[j].r, players[j].speed_x, players[j].speed_y=0,0,0
 
-                if players[i].conn != None:
-                    _x = str(round(dist_x))
-                    _y = str(round(dist_y))
-                    _r = str(round(players[j].r))
-                    _c = players[j].colour
-                    visible_players[i].append(_x+' '+_y+' '+_r+' '+_c)
-            if ((abs(dist_x) <= (players[j].w_vision) // 2 + players[i].r) and
-                    (abs(dist_y) <= (players[j].h_vision) // 2 + players[i].r)):
-                if (dist_x**2 +dist_y**2)**0.5 <= players[i].r and players[j].r > 1.1*players[i].r:
-                    players[j].r = new_r(players[j].r, players[i].r)
-                    players[i].r, players[i].speed_x, players[i].speed_y=0,0,0
-                if players[j].conn!=None:
-                    _x = str(round(-dist_x))
-                    _y = str(round(-dist_y))
-                    _r = str(round(players[i].r))
-                    _c = players[i].colour
-                    visible_players[j].append(_x + ' ' + _y + ' ' + _r + ' ' + _c)
+            if ((abs(dist_x)<= (players[i].w_vision)//2+players[j].r) and (abs(dist_y)<= (players[i].h_vision)//2+players[j].r)):
+                if (dist_x**2+dist_y**2)**0.5 <= players[i].r and players[i].r> 1.1*players[j].r:
+                    #change r
+                    players[j].r, players[j].speed_y, players[j].speed_x = 0 , 0 , 0
+
+                _x = str(round(dist_x))
+                _y = str(round(dist_y))
+                _r = str(round(players[j].r))
+                _c = players[j].colour
+                visible_players[i].append(_x+' '+_y+' '+_r+' '+_c)
+            if ((abs(dist_x) <= (players[j].w_vision) // 2 + players[i].r) and (
+                    abs(dist_y) <= (players[j].h_vision) // 2 + players[i].r)):
+                if (dist_x**2+dist_y**2)**0.5 <= players[j].r and players[j].r> 1.1*players[i].r:
+                    #change r
+                    players[i].r, players[i].speed_y, players[i].speed_x = 0 , 0 , 0
+                _x = str(round(-dist_x))
+                _y = str(round(-dist_y))
+                _r = str(round(players[i].r))
+                _c = players[i].colour
+                visible_players[j].append(_x + ' ' + _y + ' ' + _r + ' ' + _c)
 
     responce = ['' for i in range (len(players))]
-
     for i in range(len(players)):
-        _r = str(round(players[i].r))
-        visible_players[i]=[_r] + visible_players[i]
         responce[i] = '<'+(','.join(visible_players[i]))+'>'
 
     for i in range(len(players)):
-        if players[i].conn != None:
-            try:
-                players[i].conn.send(responce[i].encode())
-                players[i].errors = 0
-            except:
-                players[i].errors += 1
+        try:
+            players[i].conn.send(responce[i].encode())
+            print(responce[i])
+            players[i].errors = 0
+        except:
+            players[i].errors += 1
 
     for p in players:
-        if (p.errors == 1000) or (p.r==0):
-            if p.conn!=0:
-                p.conn.close()
+        if p.errors == 500 or p.r == 0:
+            p.conn.close()
             players.remove(p)
-
-    for m in microbes:
-        if m.r==0:
-            microbes.remove(m)
 
     sc.fill('grey15')
     for p in players:
